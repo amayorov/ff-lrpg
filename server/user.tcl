22425936d59ea36ac804c142157137dc9b5a850a
Anton Mayorov <a.s.mayorov@gmail.com>
2011-01-31 15:11:20 +0300

    Added user command evaluation function.

--- server/user.tcl
+++ server/user.tcl
@@ -1,26 +1,72 @@
 package require Tcl 8.5
+source object.tcl
+
+namespace eval test {
+    namespace export a
+    proc a {} {
+	puts a
+    }
+    proc b {} {
+	puts b
+    }
+}
 
 # Файл содержит одну-единственную команду user
 # Через неё делается всё...
 namespace eval user {
-    namespace export run
+    namespace export run allow deny
     namespace ensemble create
 
-    namespace eval ui {
-	namespace export engine test
-	namespace ensemble create
-	proc test {} {
-	    puts [shipname]
+    set allowed {}
+
+    proc shipname {ship} {
+	return $ship
+    }
+
+    proc allow {cmd {alias {}} args} {
+	variable allowed
+	if {$alias != {} } {
+	    lappend allowed [list $cmd $alias $args]
+	} else {
+	    lappend allowed [list $cmd $cmd $args]
+	}
+    }
+
+    proc deny {cmd} {
+	variable allowed
+	set idx [lsearch -all -index 0 $allowed $cmd]
+	foreach i $idx {
+	    set allowed [lreplace $allowed $i $i]
 	}
     }
 
     proc run {ship cmd} {
+	variable allowed
 	interp create -safe $ship
-	$ship alias shipname ::user::shipname $ship
-	$ship alias ui ::user::ui
-	interp eval $ship $cmd
+	# выключить потом, нечего гадить в stdout
+	interp share {} stdout $ship
+# Может, вообще set выключить, заодно и шаманить с внутренним состоянием сервера не получится...
+#	$ship alias ::user::shipname ::user::shipname $ship
+	foreach i $allowed {
+	    $ship alias [lindex $i 0] [lindex $i 1] {*}[subst -nocommands [lindex $i 2]]
+	}
+	set ns [interp eval $ship namespace children]
+	set aliases [interp eval $ship interp aliases]
+	foreach n $ns {
+	    if {$n == "::tcl"} { continue }
+	    # спорный вопрос, надо бы потестить на предмет взлома
+	    interp eval $ship namespace inscope $n {namespace export *}
+	    interp eval $ship namespace inscope $n {namespace ensemble create}
+	}
+	catch {interp eval $ship $cmd} result
 	interp delete $ship
+	return result
     }
 }
 
-user run "foo" shipname
+user allow ::user::shipname ::user::shipname {$ship}
+user allow ::test::b
+
+user run "foo" {test b}
+user run "foo" {puts [user shipname]}
+user run "foo" {user deny}
